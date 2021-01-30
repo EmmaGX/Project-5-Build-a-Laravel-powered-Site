0984c6ddb2c289f4db197432620390f31ab6adfe
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2021-01-30 13:59:02 -0500

    creates index, cats and dogs routes and appcontroller

--- pet-adoption-laravel-app/pet-adoption/routes/web.php
+++ pet-adoption-laravel-app/pet-adoption/routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/cats', function () {
+    return view('cats');
+});
+
+Route::get('/dogs', function () {
+    return view('dogs');
+});
+
+
