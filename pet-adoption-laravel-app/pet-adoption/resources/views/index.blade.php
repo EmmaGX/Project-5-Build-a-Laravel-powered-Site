ecfb551c9a5b8543f6d793417bbf65b81b127df5
Emma Gomez Xolo <emmagomezxolo@gmail.com>
2021-02-01 13:29:31 -0500

    adds layout blade. cleans up comments

--- pet-adoption-laravel-app/pet-adoption/resources/views/index.blade.php
+++ pet-adoption-laravel-app/pet-adoption/resources/views/index.blade.php
@@ -1,83 +1,68 @@
-<!doctype html>
-<html lang="en">
+@extends('layout')
+@section('body')
 
-// Adds head.blade.php to the page
-@include('head')
-
-<!-- START body -->
-<body style="background-color: #4d4d4d !important">
-
-// Adds the nav.blade.php file to the page
-@include('nav')
-    <!-- START main -->
-    <main role="main" class="container">
-        <div class="row mb-5">
-            <div class="col-12">
-                 <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/800x400/?dog,cat" alt="Card image cap">
-                    <div class="card-footer text-light text-center">
-                        <h2>Featured</h2>
-                    </div>
-                </div>
+<div class="row mb-5">
+    <div class="col-12">
+         <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/800x400/?dog,cat" alt="Card image cap">
+            <div class="card-footer text-light text-center">
+                <h2>Featured</h2>
             </div>
-        </div> <!-- end row -->
-        <div class="row mb-4">
-            <div class="col-lg-4 mb-4">
-                <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,1" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">Pet name</h5>
-                    </div>
-                </div>
+        </div>
+    </div>
+</div>
+<div class="row mb-4">
+    <div class="col-lg-4 mb-4">
+        <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,1" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-light">Pet name</h5>
             </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,2" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">Pet name</h5>
-                    </div>
-                </div>
+        </div>
+    </div>
+    <div class="col-lg-4 mb-4">
+        <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,2" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-light">Pet name</h5>
             </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,3" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">Pet name</h5>
-                    </div>
-                </div>
+        </div>
+    </div>
+    <div class="col-lg-4 mb-4">
+        <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,3" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-light">Pet name</h5>
             </div>
-        </div><!-- end row -->
-        <div class="row mb-4">
-            <div class="col-lg-4 mb-4">
-                <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,4" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">Pet name</h5>
-                    </div>
-                </div>
+        </div>
+    </div>
+</div>
+<div class="row mb-4">
+    <div class="col-lg-4 mb-4">
+        <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,4" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-light">Pet name</h5>
             </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,5" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">Pet name</h5>
-                    </div>
-                </div>
+        </div>
+    </div>
+    <div class="col-lg-4 mb-4">
+        <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,5" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-light">Pet name</h5>
             </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card bg-dark">
-                    <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,6" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">Pet name</h5>
-                    </div>
-                </div>
+        </div>
+    </div>
+    <div class="col-lg-4 mb-4">
+        <div class="card bg-dark">
+            <img class="card-img-top" src="https://source.unsplash.com/350x350/?dog,cat,6" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-light">Pet name</h5>
             </div>
-        </div><!-- end row -->
+        </div>
+    </div>
+</div>
+@endsection
 
 
-    </main><!-- END main -->
-
-@include('footer')
-
-</body> <!-- END Body -->
-</html>
